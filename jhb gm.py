185469b8e92311b828783570d4a8cdd3151655f4
tgisrizz <124857443+tgisrizz@users.noreply.github.com>
2023-11-10 18:00:26 +0530

    Delete jhb gm.py

--- jhb gm.py
+++
@@ -1,8 +0,0 @@
-#test
-with open ('RTG_acc_file.txt', 'r+') as f:
-       k = f.readlines()
-       for i in k:
-              for j in i:
-                     print (j)
-              
-              
